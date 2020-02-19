1be51347b70c200830d1bad7b3b0a70f68aebbc5
LakshmiKendre <lakshmi.patil.nine@gmail.com>
2020-02-19 18:52:22 +0530

    Refactor: taken mood message in Constructor

--- src/main/java/moodAnalyzer/MoodAnalyzer.java
+++ src/main/java/moodAnalyzer/MoodAnalyzer.java
@@ -3,18 +3,24 @@ package moodAnalyzer;
 import exception.MoodAnalyserException;
 
 public class MoodAnalyzer {
+    private String mood;
 
-    public String analyzeMood(String mood) throws MoodAnalyserException {
+    public MoodAnalyzer() {
+    }
+    public MoodAnalyzer(String message) {
+        mood = message;
+    }
+
+    public String analyzeMood() throws MoodAnalyserException {
 
         try {
             if (mood.contains("")) {
                 throw new MoodAnalyserException("Mood can not be Empty");
             }
-            if (mood.contains("Happy")) {
+            if (mood.contains("I am Happy")) {
                 return "Happy";
             }
             return "Sad";
-
         }
         catch(NullPointerException e){
             throw new MoodAnalyserException("Enter valid Mood");
--- src/test/java/moodAnalyzer/MoodAnalyzerTest.java
+++ src/test/java/moodAnalyzer/MoodAnalyzerTest.java
@@ -2,54 +2,65 @@ package moodAnalyzer;
 
 import exception.MoodAnalyserException;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MoodAnalyzerTest {
-    private MoodAnalyzer moodAnalyzer;
-    @Before
-    public void setUp() throws Exception {
-        moodAnalyzer=new MoodAnalyzer();
-    }
+    //private MoodAnalyzer moodAnalyze;
     @Test
     public void givenHappyMood_shouldReturnHappy() {
         String mood = null;
         try {
-            mood = moodAnalyzer.analyzeMood("Happy");
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Happy");
+            mood = moodAnalyzer.analyzeMood();
             Assert.assertEquals("Happy", mood);
         } catch (MoodAnalyserException e) {
             e.printStackTrace();
         }
-
     }
 
     @Test
     public void givenSadMood_shouldReturnSad() {
         String mood = null;
         try {
-            mood = moodAnalyzer.analyzeMood("Sad");
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Sad");
+            mood = moodAnalyzer.analyzeMood();
             Assert.assertEquals("Sad",mood);
         } catch (MoodAnalyserException e) {
             e.printStackTrace();
         }
-
     }
     @Test
-    public void givenNullMood_shouldReturnNull() {
+    public void givenNullMood_shouldReturnMessage() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        try {
+            ExpectedException expectedException=ExpectedException.none();
+            expectedException.expect(MoodAnalyserException.class);
+
+            String  mood = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("Enter valid Mood", mood);
+        } catch (MoodAnalyserException e) {
+           e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenEmptyMood_shouldReturnMessage() {
         String mood = null;
         try {
-            mood = moodAnalyzer.analyzeMood(null);
-            Assert.assertEquals("Happy", mood);
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("");
+            mood = moodAnalyzer.analyzeMood();
         } catch (MoodAnalyserException e) {
-            e.printStackTrace();
+            Assert.assertEquals("Mood can not be Empty",e.getMessage());
         }
-
     }
 
     @Test
-    public void givenEmptyMood_shouldReturnTrue() {
+    public void name() {
+        String mood = null;
         try {
-            String mood = moodAnalyzer.analyzeMood("");
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            mood = moodAnalyzer.analyzeMood();
             Assert.assertEquals("Happy",mood);
         } catch (MoodAnalyserException e) {
             e.printStackTrace();
